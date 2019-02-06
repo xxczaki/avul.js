c230751321224978c70c7637ce2476a0b7562ceb
maviek <mavioko7@gmail.com>
2019-02-06 23:25:46 +0100

    Update avul.c, avul.py, and 2 more files...

---
+++ avul/avul.c
@@ -0,0 +1,10 @@
+// avul.c - c version of avul.js
+
+#define nothing() NULL
+#define add(a,b) a+b
+#define sub(a,b) a-b
+#define mul(a,b) a*b
+#define div(a,b) a/b
+#define equals(a,b) a==b
+
+int main(void) { return 0; }
---
+++ avul/avul.py
@@ -0,0 +1,11 @@
+# avul.py - python version of avul.js
+
+nothing = lambda: None
+puts = print
+add = lambda a,b: a+b
+sub = lambda a,b: a-b
+mul = lambda a,b: a*b
+div = lambda a,b: a/b
+equals = lambda a,b: a==b
+Class = lambda f: f
+class AObject: pass
